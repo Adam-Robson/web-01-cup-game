e3da7f707b245d5aaf0dd67bd6d7ff92c1e2a54e
Adam Robson <adamray312@gmail.com>
2022-07-30 16:55:41 -0700

    it finally sank in to my brain

--- app.js
+++ app.js
@@ -6,29 +6,59 @@ const cupTwoEl = document.getElementById('cup-two');
 const cupThreeEl = document.getElementById('cup-three');
 const resetButtonEl = document.getElementById('reset-button');
 const startOverBtn = document.getElementById('start-over');
-
 let correctGuesses = document.getElementById('correct-guesses');
 let totalGuesses = document.getElementById('total-guesses');
 let incorrectGuesses = document.getElementById('incorrect-guesses');
 
 const cupImages = [cupOneEl, cupTwoEl, cupThreeEl];
-let buttons = [buttonOne, buttonTwo, buttonThree];
 
 let correctGuess = 0;
 let incorrectGuess = 0;
 let totalGuess = 0;
 
+function resetCups() {
+    cupOneEl.src = './assets/graycup.png';
+    cupTwoEl.src = './assets/graycup.png';
+    cupThreeEl.src = './assets/graycup.png';
+}
+
+function getRandomCup() {
+    const answer = Math.floor(Math.random() * 3);
+    return answer;
+}
+
+function handleGuess(userGuess, rightAnswer) {
+    resetCups();
+    getRandomCup();
+    cupImages[rightAnswer].src = './assets/graycupguy.png';
+    if (userGuess === rightAnswer) {
+        correctGuess++;
+        totalGuess++;
+        correctGuesses.textContent = correctGuess;
+        totalGuesses.textContent = totalGuess;
+    } else {
+        incorrectGuess++;
+        totalGuess++;
+        incorrectGuesses.textContent = incorrectGuess;
+        totalGuesses.textContent = totalGuess;
+    }
+    buttonOne.disabled = true;
+    buttonTwo.disabled = true;
+    buttonThree.disabled = true;
+
+}
+
 buttonOne.addEventListener('click', () => {
-    handleGuess(0);
-});
+    handleGuess(0, getRandomCup());
+}),
 
 buttonTwo.addEventListener('click', () => {
-    handleGuess(1);
-});
+    handleGuess(1, getRandomCup());
+}),
 
 buttonThree.addEventListener('click', () => {
-    handleGuess(2);
-});
+    handleGuess(2, getRandomCup());
+}),
 
 resetButtonEl.addEventListener('click', () => {
     cupOneEl.src = 'assets/graycup.png';
@@ -50,38 +80,3 @@ startOverBtn.addEventListener('click', () => {
     incorrectGuesses.textContent = 0;
     totalGuesses.textContent = 0;
 });
-
-
-// finally sank in
-function getRandomItem(buttons) {
-    return buttons[Math.floor(Math.random() * buttons.length)];
-}
-
-
-
-function handleGuess(userGuess, rightAnswer) {
-    resetStyles();
-    const randomIndex = Math.floor(Math.random() * cupImages.length);
-    cupImages[randomIndex].src = './assets/graycupguy.png';
-    rightAnswer = getRandomItem(buttons);
-    if (userGuess === rightAnswer) {
-        correctGuess++;
-        correctGuesses.textContent = correctGuess;
-        totalGuess++;
-        totalGuesses.textContent = totalGuess;
-    } else {
-        incorrectGuess++;
-        incorrectGuesses.textContent = incorrectGuess;
-        totalGuess++;
-        totalGuesses.textContent = totalGuess;
-    }
-}
-
-function resetStyles() {
-    cupOneEl.src = './assets/graycup.png';
-    cupTwoEl.src = './assets/graycup.png';
-    cupThreeEl.src = './assets/graycup.png';
-    buttonOne.disabled = false;
-    buttonTwo.disabled = false;
-    buttonThree.disabled = false;
-}
